61fff377dd9944613fe938093dd9eda177c34cb4
Aman <aman9458singh@gmail.com>
2023-10-24 05:04:47 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"
 
 const connectDb = ()=>{mongoose.connect(process.env.MONGO_URI,{dbName:"backendapi"})
-.then(()=>{console.log("database connected")})
+.then((c)=>{console.log(`database connected on ${c.connection.host}`)})
 .catch((e)=>{console.log(e)})
 }
 
